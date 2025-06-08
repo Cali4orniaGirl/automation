6c59d8a281e52a3c1d7ad5968eb36d67ed667fce
Cali4orniaGirl <strekalinwork@gmail.com>
2025-06-08 04:06:00 +0300

    Added first login test

---
+++ MyTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MyTests
+{
+    public class LoginTest
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();                
+        }
+
+        [Test]
+        public void isLoginSuccessfull()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
+            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            driver.FindElement(By.CssSelector("#login > button")).Click();
+
+            string message = driver.FindElement(By.Id("flash-messages")).Text;
+
+            Assert.That(message, Does.Contain("You logged into a secure area!"));
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Dispose();
+        }
+    }
+}
