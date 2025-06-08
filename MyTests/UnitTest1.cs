288c925c4f2568b5f483d81f495949e783083db8
Cali4orniaGirl <strekalinwork@gmail.com>
2025-06-08 13:12:58 +0300

    Added different cases

--- MyTests/UnitTest1.cs
+++ MyTests/UnitTest1.cs
@@ -11,23 +11,32 @@ namespace MyTests
         [SetUp]
         public void Setup()
         {
-            driver = new ChromeDriver();                
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
         }
 
-        [Test]
-        public void isLoginSuccessfull()
+        [TestCase("tomsmith", "SuperSecretPassword!", true)]
+        [TestCase("tomsmith","123", false)]
+        [TestCase("123","SuperSecretPassword!", false)]
+        public void LoginTests(string username, string password, bool isPassed)
         {
+
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
 
-            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
-            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            driver.FindElement(By.Id("username")).SendKeys(username);
+            driver.FindElement(By.Id("password")).SendKeys(password);
             driver.FindElement(By.CssSelector("#login > button")).Click();
 
-            string message = driver.FindElement(By.Id("flash-messages")).Text;
-
-            Assert.That(message, Does.Contain("You logged into a secure area!"));
+            string message = driver.FindElement(By.Id("flash")).Text;
+            if (isPassed)
+            {
+                Assert.That(message, Does.Contain("You logged into a secure area!"));
+            }
+            else
+                Assert.That(message, Does.Contain("Your password is invalid!").Or.Contains("Your username is invalid!"));
         }
 
         [TearDown]
